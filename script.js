c203c7c6bb8548a835261cbb06670670a850b4d3
Your name <Your email>
2025-04-11 11:46:28 +0200

    whatyearisit-frontend

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+fetch('https://whatyearisit-backend-inky-chi.vercel.app/year')
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('#year').textContent = data.now;
+    })
